a6f7a36faa63e642f3c116aab8803838a403e909
Aaron O'Leary <aaron.oleary@gmail.com>
2014-11-06 13:01:06 +0000

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except (IOError, ImportError):
 
 setup(
     name="pandoc-attributes",
-    version='0.1.1',
+    version='0.1.2',
     description="An Attribute class to be used with pandocfilters",
     long_description=long_description,
     py_modules=['pandocattributes'],
