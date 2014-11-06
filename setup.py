35cabbdaebed2e78371565a4f882d5b7ec2982be
Aaron O'Leary <aaron.oleary@gmail.com>
2014-11-06 12:05:14 +0000

    init

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = ''
+
+setup(
+    name="pandoc-attributes",
+    version='0.1',
+    description="An Attribute class to be used with pandocfilters",
+    long_description=long_description,
+    py_modules=['pandocattributes'],
+    author="Aaron O'Leary",
+    author_email='dev@aaren.me',
+    license='BSD 2-Clause',
+    url='http://github.com/aaren/pandoc-attributes',
+    install_requires=['pandocfilters', ],
+)
